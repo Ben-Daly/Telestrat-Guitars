55d137a374e8046b08a582db9c95bb028f53e764
Ben Daly <bendaly2000@hotmail.com>
2016-12-04 20:30:48 +0000

    Add files via upload

---
+++ css/topnav.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Ben on 03/12/2016.
+ */
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
